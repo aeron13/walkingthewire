e1aa9161a00fa41337ca3e81c8673635ed0c98df
Ready Steady <readysteadyitalia@users.noreply.github.com>
2019-04-01 19:20:17 +0200

    on click

--- wtw.js
+++ wtw.js
@@ -13,12 +13,13 @@ function giocoWTW() {/**
       game.load.baseURL = './assets';
       game.load.crossOrigin = 'anonymous';
 
-      game.load.spritesheet('player', '/spritesheet1.png', 800, 870, 19);
+      game.load.spritesheet('player', '/spritesheet1.png', 800, 870, 42);
       game.load.image('line', '/line.png');
 
   }  // end of preload function
 
   var player;
+  var controlClick = 0;
 
   function create() {
 
@@ -35,19 +36,29 @@ function giocoWTW() {/**
     player.x = game.world.centerX-player.width/2;
     //animations
     player.animations.add('walk', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12], 13, true);
-    player.animations.add('crouch', [13, 14, 15, 16, 17, 18, 19], 10, false);
+    player.animations.add('crouch', [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 ,24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34 ,35 ,36, 37, 38, 39, 40, 41, 42], 15, false);
     player.animations.play('walk');
     //click
     player.inputEnabled = true;
-    player.events.onInputDown.add(listener, this);
+    player.events.onInputDown.add(onClick, this);
     /*player.width = 100;
     player.height = 100;*/
     //player.anchor.setTo(0.5);
 
   }   // end of create function
 
-function listener() {
-  player.animations.play('crouch');
+//cambia la animation
+function onClick() {
+  if(controlClick == 0) {
+    controlClick = 1;
+    player.animations.play('crouch');
+
+    //callback
+    setTimeout(function () {
+      player.animations.play('walk');
+      controlClick = 0;
+    }, 2000)
+  }
 }
 
 
